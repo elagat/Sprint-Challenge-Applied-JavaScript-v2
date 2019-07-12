ba3ea1880a966a8287b3634dc1a7829908a2aea7
Emily Lagattolla <elagattolla@gmail.com>
2019-07-12 11:38:21 -0500

    WIP: creating tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -9,14 +9,33 @@
 //    <div class="tab">topic here</div>
 
 const tabs = document.querySelector('.tabs');
+const topics = document.querySelector('.topics');
 
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
   .then(res => {
     console.log(res.data);
-    const tab = createtTab(res.data);
+    const tab = createTab(res.data);
     tabs.append(tab);
   })
 
-function createTab() {
+const topicsArray = [
+  'javascript',
+  'bootstrap',
+  'technology',
+  'jquery',
+  'node.js',
+];
 
+topicsArray.forEach(topic => {
+  axios.get(`https://lambda-times-backend.herokuapp.com/${topic}`)
+    .then(res => {
+      const topic = createTab(res.data);
+      topics.append(topic);
+    })
+})
+
+function createTab() {
+  const tabs
 }
+
+tabs.appendChild(createTab());
